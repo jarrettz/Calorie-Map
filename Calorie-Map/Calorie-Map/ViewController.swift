a5c08b4c69be3435f00e5f6d6d6632fd33981eec
michaelsatumba <mjsatumba@gmail.com>
2022-04-07 10:36:50 -0700

    google maps sdk

--- Calorie-Map/Calorie-Map/ViewController.swift
+++ Calorie-Map/Calorie-Map/ViewController.swift
@@ -6,9 +6,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("hello")
+        print("Google Maps SDK")
     }
-
+    
 
 }
 
