a1b652a779643a59889c1dd220f596ecc9895392
michaelsatumba <mjsatumba@gmail.com>
2022-04-08 11:06:20 -0700

    SF 📍

--- Calorie-Map/Calorie-Map/ViewController.swift
+++ Calorie-Map/Calorie-Map/ViewController.swift
@@ -8,13 +8,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Maps"
+        title = "Calorie Map"
         view.addSubview(mapView)
+
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         mapView.frame = view.bounds
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 37.77, longitude: -122.43)
+        annotation.title = "SF"
+        mapView.addAnnotation(annotation)
+        let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 50000, longitudinalMeters: 50000)
+        mapView.setRegion(region, animated: true)
     }
     
 
