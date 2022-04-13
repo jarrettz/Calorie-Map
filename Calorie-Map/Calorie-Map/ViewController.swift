0e5107928d2f8d61195eeef407671f4d0212d5d2
michaelsatumba <mjsatumba@gmail.com>
2022-04-12 21:10:48 -0700

    what i have

--- Calorie-Map/Calorie-Map/ViewController.swift
+++ Calorie-Map/Calorie-Map/ViewController.swift
@@ -2,48 +2,94 @@
 import UIKit
 import MapKit
 import FloatingPanel
+import CoreLocation
 
-class ViewController: UIViewController, UISearchResultsUpdating {
+class ViewController: UIViewController {
   
+    // Michael: - Properties
     
+    var mapView: MKMapView!
+    var locationManager: CLLocationManager!
     
-    let mapView = MKMapView()
-    
-    let searchVC = UISearchController(searchResultsController: ResultsViewController())
+//    let searchVC = UISearchController(searchResultsController: ResultsViewController())
 
+    // Michael: - Init
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Calorie Map"
+        configureViewComponents()
+        enableLocationServices()
+//        title = "Calorie Map"
+//        view.addSubview(mapView)
+//        searchVC.searchBar.backgroundColor = .secondarySystemBackground
+//        searchVC.searchResultsUpdater = self
+//        navigationItem.searchController = searchVC
+//
+//        let panel = FloatingPanelController()
+//        panel.set(contentViewController: SearchViewController())
+//        panel.addPanel(toParent: self)
+
+    }
+    
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        mapView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.frame.size.width, height: view.frame.size.height - view.safeAreaInsets.top)
+//        let annotation = MKPointAnnotation()
+//        let annotationTwo = MKPointAnnotation()
+//        annotation.coordinate = CLLocationCoordinate2D(latitude: 37.77, longitude: -122.43)
+//        annotationTwo.coordinate = CLLocationCoordinate2D(latitude: 38.77, longitude: -122.43)
+//        mapView.addAnnotation(annotation)
+//        mapView.addAnnotation(annotationTwo)
+//        let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 500000, longitudinalMeters: 500000)
+//        mapView.setRegion(region, animated: true)
+//    }
+    
+    // Michael: - Helper Functions
+    
+    func configureViewComponents() {
+        view.backgroundColor = .white
+        configureMapView()
+    }
+    
+    func configureMapView() {
+        mapView = MKMapView()
+        mapView.showsUserLocation = true
+//        mapView.delegate = self
+        mapView.userTrackingMode = .follow
+//
         view.addSubview(mapView)
-        searchVC.searchBar.backgroundColor = .secondarySystemBackground
-        searchVC.searchResultsUpdater = self
-        navigationItem.searchController = searchVC
-        
-        let panel = FloatingPanelController()
-        panel.set(contentViewController: SearchViewController())
-        panel.addPanel(toParent: self)
-
+        mapView.addConstraintsToFillView(view: view)
     }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        mapView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.frame.size.width, height: view.frame.size.height - view.safeAreaInsets.top)
-        let annotation = MKPointAnnotation()
-        let annotationTwo = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(latitude: 37.77, longitude: -122.43)
-        annotationTwo.coordinate = CLLocationCoordinate2D(latitude: 38.77, longitude: -122.43)
-        mapView.addAnnotation(annotation)
-        mapView.addAnnotation(annotationTwo)
-        let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 500000, longitudinalMeters: 500000)
-        mapView.setRegion(region, animated: true)
-    }
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        
-    }
-    
-
 }
+// Michael: - CLLocationManagerDelegate
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func enableLocationServices() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            print("Location auth status is NOT DETERMINED")
+            
+//            DispatchQueue.main.async {
+//                let controller = LocationRequestController()
+//                controller.locationManager = self.locationManager
+//                self.present(controller, animated: true, completion: nil)
+//            }
+            
+        case .restricted:
+            print("Location auth status is RESTRICTED")
+        case .denied:
+            print("Location auth status is DENIED")
+        case .authorizedAlways:
+            print("Location auth status is AUTHORIZED ALWAYS")
+        case .authorizedWhenInUse:
+            print("Location auth status is AUTHORIZED WHEN IN USE")
+        }
+    }
+}
+
 
 // TO DO
 /*
